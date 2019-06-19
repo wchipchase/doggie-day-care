3022839bc5b9a2fd9287213cea98349248c4caa7
wchipchase <wayne.chipchase@gmail.com>
2019-06-19 18:01:00 -0500

    working on employee

--- src/components/StaffRoom/StaffRoom.js
+++ src/components/StaffRoom/StaffRoom.js
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import Employee from '../Employee/Employee';
+import employeeShape from '../../helpers/propz/employeeShape';
 
 class StaffRoom extends React.Component {
+  static propTypes = {
+    employees: PropTypes.arrayOf(employeeShape.employeeShape),
+  }
+  
   render() {
     const { employees } = this.props;
     const makeEmployees = employees.map(employee => (
